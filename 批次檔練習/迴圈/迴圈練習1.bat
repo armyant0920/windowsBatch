0476d5e7648986e83d6774f549a6b9825787904f
armyant0920 <armyant0920@gmail.com>
2022-09-01 23:08:40 +0800

    Update 迴圈練習1.bat
    使用/R 準備結合net use看看

--- 批次檔練習/迴圈/迴圈練習1.bat
+++ 批次檔練習/迴圈/迴圈練習1.bat
@@ -2,10 +2,11 @@
 ECHO %%~dp0 = %~dp0
 ::ECHO %%cd%%  = %cd%
 set path=%~dp0
-
-for /R %path% %%G in (*) do echo "%%G"
-
-    
+echo ===============
+for /R %path% %%G in (*.txt) do echo "%%G"
+echo ===============
+for /R "C:\Users\user\OneDrive\文件\GitHub\windowsBatch\批次檔練習\測試創建並移動檔案 - 複製" %%G in (*) do echo %%G
+   
 pause	
 ::for %i in (%userprofile%\*) DO @echo %i
 ::for /D %i in (%userprofile%\*) DO @echo %i
